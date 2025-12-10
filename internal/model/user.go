8db61212d99127d5832f422b38e4f66eaf115cba
juancwu <46619361+juancwu@users.noreply.github.com>
2025-12-10 16:22:11 -0500

    add user model

---
+++ internal/model/user.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type User struct {
+	ID    string `db:"id"`
+	Email string `db:"email"`
+	// Allow null for passwordless users
+	PasswordHash    *string    `db:"password_hash"`
+	PendingEmail    *string    `db:"pending_email"`
+	EmailVerifiedAt *time.Time `db:"email_verified_at"`
+	CreatedAt       time.Time  `db:"created_at"`
+}
+
+func (u *User) HasPassword() bool {
+	return u.PasswordHash != nil && *u.PasswordHash != ""
+}
