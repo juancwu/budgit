2f1a7154e446cc2a63678355f06b5757ccc496dd
juancwu <git@juancwu.dev>
2026-02-18 14:57:35 +0000

    fix: redirect middleware return type

--- internal/middleware/redirect.go
+++ internal/middleware/redirect.go
@@ -2,7 +2,7 @@ package middleware
 
 import "net/http"
 
-func Redirect(path string) http.Handler {
+func Redirect(path string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("HX-Request") == "true" {
 			w.Header().Set("HX-Redirect", path)
